6316e4d5c1d3d260560bc6bdfa3256d44a36de30
lucashuet93 <lucashh@microsoft.com>
2019-03-06 13:34:01 -0500

    sends number of queue messages to service bus

--- ServiceBusQueueMessageSender/Program.cs
+++ ServiceBusQueueMessageSender/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using System.Text;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 
 namespace ServiceBusQueueMessageSender
 {
     class Program
     {
+        const string ServiceBusConnectionString = "Endpoint=sb://iseapim.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ZjSn6Aaper6y56U4/UIGO6spm3ct+UzNIqEX4VlGP/E=";
+        const string QueueName = "sbq";
+        static IQueueClient queueClient;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            const int numberOfMessages = 1;
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
         }
     }
 }
