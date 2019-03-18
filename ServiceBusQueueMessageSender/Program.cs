5bc20abd6f18951b139ad5c53fcdfa7914ff6024
lucashuet93 <lucashh@microsoft.com>
2019-03-18 15:49:21 -0400

    rm creds

--- ServiceBusQueueMessageSender/Program.cs
+++ ServiceBusQueueMessageSender/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System;
 using System.Threading;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.ServiceBus;
 
@@ -8,8 +9,8 @@ namespace ServiceBusQueueMessageSender
 {
     class Program
     {
-        const string ServiceBusConnectionString = "Endpoint=sb://iseapim.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ZjSn6Aaper6y56U4/UIGO6spm3ct+UzNIqEX4VlGP/E=";
-        const string QueueName = "sbq";
+        const string ServiceBusConnectionString = "";
+        const string QueueName = "";
         static IQueueClient queueClient;
 
         static void Main(string[] args)
@@ -34,8 +35,16 @@ namespace ServiceBusQueueMessageSender
             {
                 for (var i = 0; i < numberOfMessagesToSend; i++)
                 {
+                    MessageBody body = new MessageBody() {
+                        CallTypeId = 1,
+                        KeyId1 = "foo",
+                        KeyId2 = 1,
+                        TransactionTypeId = 1
+                    };
+
+                    string messageBody = JsonConvert.SerializeObject(body);
+
                     // Create a new message to send to the queue.
-                    string messageBody = $"Message {i}";
                     var message = new Message(Encoding.UTF8.GetBytes(messageBody));
 
                     // Write the body of the message to the console.
@@ -50,5 +59,12 @@ namespace ServiceBusQueueMessageSender
                 Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
             }
         }
+    }
+
+    public class MessageBody {
+        public int CallTypeId { get; set; }
+        public string KeyId1 { get; set; }
+        public int KeyId2 { get; set; }
+        public int TransactionTypeId { get; set; }
     }
 }
